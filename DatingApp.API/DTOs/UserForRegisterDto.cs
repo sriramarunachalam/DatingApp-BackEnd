cb622541fdaaaa8aaa6033e1cce56ed4ccb46791
Sriram Arunachalam <sriramarunaachalam@gmail.com>
2019-09-29 07:36:26 -0400

    Login and Register

---
+++ DatingApp.API/DTOs/UserForRegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.DTOs
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "Password must be between 4 and 8 characters")]
+        public string Password { get; set; }
+    }
+}
